3d21727acd4529861c6e3e5ace3f6c007e2f684f
Pratik <pratikpawar2703@gmail.com>
2023-08-13 19:56:44 +0530

    .

--- 2) Recursion/8 (1) Taylor series.c
+++ 2) Recursion/8 (1) Taylor series.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
-int e(int x,int n){
-    static int p=1, f=1;
-    int r;
-    if(n==0)
-        return 1;
-    else 
-    r=e(x,n-1);
-    p=p*x;
-    f=f*n;
-    return r+(p/f);
+
+double e(int x, int n)
+{
+ static double p=1,f=1;
+ double r;
+ 
+ if(n==0)
+ return 1;
+ r=e(x,n-1);
+ p=p*x;
+ f=f*n;
+ return r+p/f;
 }
-int main(){
-    int x= e(1,2);
-    printf("%d",x);
+int main()
+{
+ printf("%lf \n",e(2,10));
+ return 0;
 }
--- 2) Recursion/8 (2) Taylor series.c
+++ 2) Recursion/8 (2) Taylor series.c
@@ -1,25 +1,16 @@
 //faster approch
 #include<stdio.h>
-
-int e(int x,int n){
-    static int s=1;
-    if(n==0)
-        return 1;
-    s=1+(x/n)*s;
-    return e(x,n-1);
+double e(int x, int n)
+{
+ static double s;
+ if(n==0)
+ return s;
+ s=1+x*s/n;
+ return e(x,n-1);
+ 
 }
-int Ie(int x,int n){
-    if(n==0)
-        return 1;
-    int s=1;
-    for(;n>0 ; n--){
-        s = 1+(x/n)*s;
-    }
-    return s;
-}
-int main(){
-    int x= e(1,2);
-    printf("%d",x);
-    x=Ie(1,2);
-    printf("%d",x);
+int main()
+{
+ printf("%lf \n",e(2,10));
+ return 0;
 }
