4b6173d973c2db544c561aab65a0f1a90479c40a
Pratik <pratikpawar2703@gmail.com>
2023-08-19 00:09:27 +0530

    Static Array

--- 03) Arrays Representations/02. Static Array.c
+++ 03) Arrays Representations/02. Static Array.c
@@ -4,9 +4,27 @@
 int main(){
     //Sizwe of array is decided at compile time but memory allocation is done at runtime only
     //in C language array size must be constant integer, we cannot place variable or anything else like C++
-    int A[2]={1,2,3,4,5}; //normal array Memory allocated at Stack
+    int A[5]={1,2,3,4,5}; //normal array Memory allocated at Stack
     int *p;     //required for heap
-    p = (int *)malloc(5*sizeof(int));
+    p = (int *)malloc(5*sizeof(int)); //created array of size 5 in Heap using malloc function
+    p[0]=6;
+    p[1]=7;
+    p[2]=8;
+    p[3]=9;
+    p[4]=10;
+
+    printf("\nArray in Stack : \n");
+    for(int i=0 ; i<5 ; i++){
+        printf("%d\n",A[i]);
+    }
+
+    printf("\nArray in Heap : \n");
+    for(int i=0 ; i<5 ; i++){
+        printf("%d\n",p[i]);
+    }
+
+    free(p);// after using Heap memory we should release it, otherwise it will cost data leaks
+
 
     return 0;
 }
