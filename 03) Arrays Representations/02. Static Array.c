69ec4c9e1d6346ec029b250f14860c398ac00507
Pratik <pratikpawar2703@gmail.com>
2023-08-18 23:57:49 +0530

    Static Array

--- 03) Arrays Representations/02. Static Array.c
+++ 03) Arrays Representations/02. Static Array.c
@@ -1,2 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>      //for malloc function
+
+int main(){
+    //Sizwe of array is decided at compile time but memory allocation is done at runtime only
+    //in C language array size must be constant integer, we cannot place variable or anything else like C++
+    int A[2]={1,2,3,4,5};
+    int *p;
+
+    return 0;
+}
