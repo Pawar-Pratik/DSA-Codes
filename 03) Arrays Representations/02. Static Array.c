e966fb17de42215ab79616f36aa7e212163c9a8e
Pratik <pratikpawar2703@gmail.com>
2023-08-18 23:58:40 +0530

    Static Array

--- 03) Arrays Representations/02. Static Array.c
+++ 03) Arrays Representations/02. Static Array.c
@@ -4,8 +4,8 @@
 int main(){
     //Sizwe of array is decided at compile time but memory allocation is done at runtime only
     //in C language array size must be constant integer, we cannot place variable or anything else like C++
-    int A[2]={1,2,3,4,5};
-    int *p;
+    int A[2]={1,2,3,4,5}; //normal array Memory allocated at Stack
+    int *p;     //required for heap
 
     return 0;
 }
